fb256c9b2920659000af2c8e2e60b8ab763b6381
Tyler Carbone <tcarbone073@gmail.com>
2023-04-22 10:35:12 +0000

    Add basic models

--- app/models.py
+++ app/models.py
@@ -60,3 +60,82 @@ class CreatedOnMixin:
     """
 
     createdon = sa.Column(sa.DateTime(), server_default=sa.func.now())
+
+
+class User(DeclBase, IDMixin, CreatedOnMixin):
+    # Table args
+
+    # Columns
+    firstname = sa.Column(sa.String(Config.DB_SHRT_STRING), nullable=False)
+    middlename = sa.Column(sa.String(Config.DB_SHRT_STRING))
+    lastname = sa.Column(sa.String(Config.DB_SHRT_STRING), nullable=False)
+    email = sa.Column(sa.String(Config.DB_SHRT_STRING), nullable=False, unique=True)
+
+    # Relationships
+    _password = sa_orm.relationship("Password", back_populates="_user", uselist=False, cascade="all, delete-orphan")
+    _apitoken = sa_orm.relationship("APIToken", back_populates="_user", uselist=False, cascade="all, delete-orphan")
+
+
+class Password(DeclBase):
+    # Table args
+
+    # Columns
+    user_id = sa.Column(sa.Integer(), sa.ForeignKey("User.id"), nullable=False)
+    hash = sa.Column(sa.String(128), nullable=False)
+    salt = sa.Column(sa.String(5), nullable=False)
+
+    # Relationships
+    _user = sa_orm.relationship("User", back_populates="_password")
+
+
+class APIToken(DeclBase, CreatedOnMixin):
+    # Table args
+
+    # Columns
+    user_id = sa.Column(sa.Integer(), sa.ForeignKey("User.id"), nullable=False)
+    token = sa.Column(sa.String(32), nullable=False)
+    expiration = sa.Column(sa.DateTime(), nullable=False)
+
+    # Relationships
+    _user = sa_orm.relationship("User", back_populates="_apitoken")
+
+
+class Facility(DeclBase, IDNameMixin):
+    # Table args
+    __tableargs__ = sa.UniqueConstraint("name")
+
+    # Relationships
+    _covidcase = sa_orm.relationship("CovidCase", back_populates="_facility")
+
+
+class Building(DeclBase, IDNameMixin):
+    # Table args
+    __tableargs__ = sa.UniqueConstraint("name")
+
+    # Relationships
+    _covidcase = sa_orm.relationship("CovidCase", back_populates="_building")
+
+
+class Department(DeclBase, IDNameMixin):
+    # Table args
+    __tableargs__ = sa.UniqueConstraint("name")
+
+    # Relationships
+    _covidcase = sa_orm.relationship("CovidCase", back_populates="_department")
+
+
+class CovidCase(DeclBase, IDMixin):
+    # Table args
+
+    # Columns
+    facility_id = sa.Column(sa.Integer(), sa.ForeignKey("Facility.id"))
+    building_id = sa.Column(sa.Integer(), sa.ForeignKey("Building.id"))
+    department_id = sa.Column(sa.Integer(), sa.ForeignKey("Department.id"))
+    last_work_date = sa.Column(sa.Date())
+    test_date = sa.Column(sa.Date())
+    post_date = sa.Column(sa.Date())
+
+    # Relationships
+    _facility = sa_orm.relationship("Facility", back_populates="_covidcase")
+    _building = sa_orm.relationship("Building", back_populates="_covidcase")
+    _department = sa_orm.relationship("Department", back_populates="_covidcase")
